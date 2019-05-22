df7dd253609d3562783eeee394b9d6b04a2c82ec
Chloie Parsons <chloieparsons@gmail.com>
2019-05-22 16:48:47 -0700

    Added edit-file.js and nested readOne fucntions in reader.js

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -5,8 +5,8 @@ module.exports = exports = {};
 exports.readFile = (file, cb) => {
   if( file.match(/bad/i) ) {
     cb('Invalid File');
-  }
-  else {
-    cb(undefined, new Buffer('File Contents'));
+  
+  }else{
+    cb(undefined, Buffer.from('File Contents'));
   }
 };

b0678446bcbb11a6747a12efb8e382e9e2ee0a21
Chloie Parsons <chloieparsons@gmail.com>
2019-05-29 16:06:36 -0700

    All files and all tests working finally

--- __tests__/lib/reader-async.test.js
+++ __tests__/lib/reader-async.test.js
@@ -2,30 +2,27 @@
 
 jest.mock('fs');
 
-const reader = require('../../lib/reader.js');
+const readerAsync = require('../../lib/reader-async.js');
 
-describe('File Reader Module', () => {
+describe('File Async Reader Module', () => {
 
-  it('when given a bad file, returns an error', done => {
-    let files = ['bad.txt'];
-    // In jest, throwing errors obviously kills the app, so if you're
-    // going to throw one in a test, have the expect execute your code as a
-    // function so that you can trap it.
-    reader(files, (err, data) => {
-      expect(err).toBeDefined();
-      done();
-    });
+  it('when given a bad file, returns an error', async () => {
+    try{
+      let result = await readerAsync(false);
+      expect(result).toBeUndefined();
+    }catch(error){
+      console.log(`In catch statement with message: ${error}`);
+    }
   });
 
 
-  it('reads 3 files', done => {
-    let files = ['file1.txt', 'file2.txt', 'file2.txt'];
-    reader(files, (err,data) => {
-      expect(err).toBeNull();
-      expect(data instanceof Array ).toBeTruthy();
-      expect(data.length ).toBe(3);
-      done();
-    });
+  it('reads 3 files', async () => {
+    let files = ['1.txt', '2.txt', '3.txt'];
+    try{
+      let result = await readerAsync(files);
+      expect(result).toEqual(files);
+    }catch(error){
+      console.log(`In catch statement with message: ${error}`);
+    }
   });
-
 });
